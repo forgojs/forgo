19b25251f3646f452c8639709edf5bc18b653597
Jeswin <jeswinpk@agilehead.com>
2021-01-15 19:16:25 +0530

    Add test for node attached state correctness

--- test/src/test.ts
+++ test/src/test.ts
@@ -3,9 +3,11 @@ import passProps from "./passProps";
 import elementRef from "./elementRef";
 import rerender from "./rerender";
 import unmount from "./unmount";
+import nodeState from "./nodeState";
 
 mount();
 passProps();
 elementRef();
 rerender();
 unmount();
+nodeState();
