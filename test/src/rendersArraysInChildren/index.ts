5e94256a97b03b0874b1bc46a901e94036894e1d
Jeswin <jeswinpk@agilehead.com>
2021-01-28 18:05:09 +0530

    Rename test - arrays in dom node children

--- test/src/rendersArraysInChildren/index.ts
+++ test/src/rendersArraysInChildren/index.ts
@@ -4,30 +4,32 @@ import "should";
 import { runArrays, runNestedArrays } from "./script";
 
 export default function rendersArraysInChildren() {
-  const tests: [string, (dom: JSDOM) => void][] = [
-    ["arrays", runArrays],
-    ["arrays", runNestedArrays],
-  ];
+  describe("renders arrays as DOM node children", () => {
+    const tests: [string, (dom: JSDOM) => void][] = [
+      ["array", runArrays],
+      ["nested array", runNestedArrays],
+    ];
 
-  tests.forEach(([testName, run]) => {
-    it(`renders ${testName} in children`, async () => {
-      const dom = new JSDOM(htmlFile(), {
-        runScripts: "outside-only",
-        resources: "usable",
-      });
-      const window = dom.window;
-
-      run(dom);
-
-      const innerHtml = await new Promise<string>((resolve) => {
-        window.addEventListener("load", () => {
-          resolve(window.document.body.innerHTML);
+    tests.forEach(([testName, run]) => {
+      it(testName, async () => {
+        const dom = new JSDOM(htmlFile(), {
+          runScripts: "outside-only",
+          resources: "usable",
         });
-      });
+        const window = dom.window;
 
-      innerHtml.should.containEql(
-        "<div>Hello world<p>1</p><p>2</p><p>3</p><p>4</p></div>"
-      );
+        run(dom);
+
+        const innerHtml = await new Promise<string>((resolve) => {
+          window.addEventListener("load", () => {
+            resolve(window.document.body.innerHTML);
+          });
+        });
+
+        innerHtml.should.containEql(
+          "<div>Hello world<p>1</p><p>2</p><p>3</p><p>4</p></div>"
+        );
+      });
     });
   });
 }
