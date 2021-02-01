dcd98a6c71d7c7faf5a6d50605bfdf3122543e18
Jeswin <jeswinpk@agilehead.com>
2021-02-01 09:50:42 +0530

    Add test for nested fragments

--- test/src/componentFragment/index.ts
+++ test/src/componentFragment/index.ts
@@ -1,27 +1,50 @@
 import { JSDOM } from "jsdom";
 import htmlFile from "../htmlFile";
-import { run } from "./script";
+import { run, runNested } from "./script";
 import "should";
 
 export default function componentUnmount() {
-  it("renders component returning a fragment", async () => {
-    const dom = new JSDOM(htmlFile(), {
-      runScripts: "outside-only",
-      resources: "usable",
-    });
-    const window = dom.window;
-
-    run(dom);
-
-    await new Promise<void>((resolve) => {
-      window.addEventListener("load", () => {
-        resolve();
+  describe("renders component returning fragments", () => {
+    it("top level fragment", async () => {
+      const dom = new JSDOM(htmlFile(), {
+        runScripts: "outside-only",
+        resources: "usable",
       });
+      const window = dom.window;
+
+      run(dom);
+
+      await new Promise<void>((resolve) => {
+        window.addEventListener("load", () => {
+          resolve();
+        });
+      });
+
+      const rootElem = window.document.getElementById("root") as HTMLElement;
+      rootElem.innerHTML.should.containEql(
+        "<div>1</div><div>2</div><div>3</div>"
+      );
     });
 
-    const rootElem = window.document.getElementById("root") as HTMLElement;
-    rootElem.innerHTML.should.containEql(
-      "<div>1</div><div>2</div><div>3</div>"
-    );
+    it("nested fragment", async () => {
+      const dom = new JSDOM(htmlFile(), {
+        runScripts: "outside-only",
+        resources: "usable",
+      });
+      const window = dom.window;
+
+      runNested(dom);
+
+      await new Promise<void>((resolve) => {
+        window.addEventListener("load", () => {
+          resolve();
+        });
+      });
+
+      const rootElem = window.document.getElementById("root") as HTMLElement;
+      rootElem.innerHTML.should.containEql(
+        "<div>1</div><div>2</div><div>3</div><div>4</div>"
+      );
+    });
   });
 }
--- test/src/componentFragment/script.tsx
+++ test/src/componentFragment/script.tsx
@@ -18,6 +18,25 @@ function Component() {
   };
 }
 
+function NestedFragmentComponent() {
+  return {
+    render(props: any, args: ForgoRenderArgs) {
+      return (
+        <>
+          <>
+            <div>1</div>
+            <div>2</div>
+          </>
+          <>
+            <div>3</div>
+          </>
+          <div>4</div>
+        </>
+      );
+    },
+  };
+}
+
 export function run(dom: JSDOM) {
   window = dom.window;
   document = window.document;
@@ -25,5 +44,15 @@ export function run(dom: JSDOM) {
 
   window.addEventListener("load", () => {
     mount(<Component />, window.document.getElementById("root"));
+  });
+}
+
+export function runNested(dom: JSDOM) {
+  window = dom.window;
+  document = window.document;
+  setCustomEnv({ window, document });
+
+  window.addEventListener("load", () => {
+    mount(<NestedFragmentComponent />, window.document.getElementById("root"));
   });
 }
