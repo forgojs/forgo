eae19e5531e50947c82b3e0612b6c9131c1c6262
spiffytech <git@spiffy.tech>
2022-07-14 18:29:53 -0400

    #39: Test that the component isn't recreated after it returns null

--- src/test/unmountsParentWhenNodeIsNull.tsx
+++ src/test/unmountsParentWhenNodeIsNull.tsx
@@ -4,28 +4,37 @@ import * as forgo from "../index.js";
 import { run } from "./componentRunner.js";
 import type { ForgoComponentCtor, ForgoComponentProps } from "../index";
 
-let renderCount = 0;
-
 const componentFactory = () => {
   const state: {
+    shouldRenderNull: boolean;
+    internalState: string | null;
     renderedElement?: ChildNode;
     update: () => void;
+    hasMounted: boolean;
     hasUnmounted: boolean;
-  } = { hasUnmounted: false, update: () => undefined };
+  } = {
+    shouldRenderNull: true,
+    internalState: null,
+    hasMounted: false,
+    hasUnmounted: false,
+    update: () => undefined,
+  };
 
   const Component: ForgoComponentCtor<ForgoComponentProps> = () => {
-    // We keep this inside the component ctor closure to test that the component
-    // doesn't get recreated when it stops rendering null
-    const internalState = Math.random().toString();
+    // We want to reassign this inside the component ctor closure to test that
+    // the component doesn't get recreated when it stops rendering null
+    state.internalState = Math.random().toString();
 
     return {
+      mount() {
+        state.hasMounted = true;
+      },
       render(_props, args) {
         state.update = args.update;
-        renderCount += 1;
-        if (renderCount % 2 === 0) {
+        if (state.shouldRenderNull) {
           return null;
         } else {
-          return <div>Internal state is {internalState}</div>;
+          return <div>Internal state is {state.internalState}</div>;
         }
       },
       afterRender(_props, args) {
@@ -43,21 +52,29 @@ export default function () {
   it("does not unmount parent when render returns null", async () => {
     const { Component, state } = componentFactory();
 
-    const { document } = await run(() => <Component />);
+    await run(() => <Component />);
 
-    const internalStateText = state.renderedElement!.textContent;
-    // Sanity checks
-    should.equal(typeof internalStateText, "string");
-    should.notEqual(internalStateText, "");
+    // Make sure that the first render mounts the component, even if it renders
+    // null
+    should.equal(state.hasMounted, true);
+    should.equal(state.hasUnmounted, false);
+    should.equal(state.renderedElement!.nodeType, 8);
 
+    const internalState = state.internalState;
+    // Sanity checks for rendering the randomized state
+    should.equal(typeof internalState, "string");
+    should.notEqual(internalState, "");
+
+    state.shouldRenderNull = true;
     state.update();
     should.equal(state.hasUnmounted, false);
     should.equal(state.renderedElement!.nodeType, 8);
 
+    state.shouldRenderNull = false;
     state.update();
     should.equal(state.hasUnmounted, false);
     should.equal(state.renderedElement!.nodeType, 1);
-    const newInternalStateText = state.renderedElement!.textContent;
-    should.equal(newInternalStateText, internalStateText);
+    const newInternalState = state.internalState;
+    should.equal(newInternalState, internalState);
   });
 }
